c8cf798c8a0eede86b19fc0cc1563184adae7fb5
exroot <exroot.dev@gmail.com>
2020-12-09 13:18:21 -0400

    Adding next, tailwind & babel configuration files

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const twinMacro = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = { fs: "empty" };
+    }
+
+    return config;
+  },
+};
+
+module.exports = {
+  ...twinMacro,
+};
